8f2248a186f6822cd4eb2b490af911e6a7ac42a3
Nikita Podvirnyi <krypt0nn@vk.com>
2024-11-15 01:41:57 +0200

    build: added lib.rs file

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+pub mod messages;
+pub mod tokens;
+pub mod tokenized_messages;
+pub mod ngram;
+pub mod dataset;
+pub mod model;
+
+pub mod cli;
+
+pub mod prelude {
+    pub use super::messages::Messages;
+
+    pub use super::tokens::{
+        Tokens,
+        START_TOKEN,
+        END_TOKEN
+    };
+
+    pub use super::tokenized_messages::TokenizedMessages;
+
+    pub use super::ngram::{
+        Ngram,
+        Unigram,
+        Bigram,
+        Trigram
+    };
+
+    pub use super::dataset::Dataset;
+    pub use super::model::params::GenerationParams;
+    pub use super::model::transitions::Transitions;
+    pub use super::model::generator::Generator;
+    pub use super::model::model::Model;
+}
